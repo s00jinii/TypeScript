6fa8dcd55a82101a22cb7c72f0e04e60cca23e85
sujin <sujinii.kim@gmail.com>
2021-01-28 13:22:10 +0900

    20210128 #TypeScript

--- learn-typescript-master/2_address-book/src/index.ts
+++ learn-typescript-master/2_address-book/src/index.ts
@@ -1,22 +1,4 @@
-interface PhoneNumberDictionary {
-  [phone: string]: {
-    num: number;
-  };
-}
-
-interface Contact {
-  name: string;
-  address: string;
-  phones: PhoneNumberDictionary;
-}
-
-enum PhoneType{
-  home = 'home',
-  address = 'office',
-  Studio = 'studio'
-}
-
-
+import { Contact, PhoneType } from './types';
 // api
 // TODO: 아래 함수의 반환 타입을 지정해보세요.
 function fetchContacts(): Promise<Contact[]> {
